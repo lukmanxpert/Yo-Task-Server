fcba741f76d9208c0076fa6e9ea5986607c1cf2f
lukmanxpert <lukmanmiah2004@gmail.com>
2025-02-21 10:16:24 +0600

    initialise the server and connect the mongodb

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l73rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+    // ended point
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// connect to server
+app.listen(port, () => {
+  console.log(`App is listening at port ${port}`);
+});
