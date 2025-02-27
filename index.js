f70088f5550b24c000191692cca2c9a3e933f6ce
lukmanxpert <lukmanmiiah2004@gmail.com>
2025-02-27 17:13:42 +0600

    finished server

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
-// mongodb start //
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l73rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,23 +19,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     const database = client.db("TaskManager");
     const usersCollection = database.collection("users");
     const taskCollection = database.collection("task");
 
     app.post('/users', async (req, res) => {
-      const usersBody = req.body
-      // new up
+      const usersBody = req.body;
       const query = { userEmail: usersBody.userEmail }
       const existingUser = await usersCollection.findOne(query)
       if (existingUser) {
         return res.send({ message: 'user already exists', insertedId: null })
       }
-      // new up
       const result = await usersCollection.insertOne(usersBody)
       res.send(result)
     })
@@ -97,8 +89,6 @@ async function run() {
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -106,7 +96,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send("Task Management ...")
+  res.send("Task Management started!!!!!")
 })
 
 app.listen(port, () => {
