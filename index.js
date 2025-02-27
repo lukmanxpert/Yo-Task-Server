00ec502c119d8aefe82b4b2aa237aa1fd75ae2fc
lukmanxpert <lukmanmiiah2004@gmail.com>
2025-02-27 13:03:31 +0600

    modyfied some bugs and setup

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express();
-app.use(cors());
-app.use(express.json());
-require("dotenv").config();
+require('dotenv').config()
+const express = require('express');
+const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const port = process.env.PORT || 7000
+app.use(express.json())
+app.use(cors())
+
+// mongodb start //
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l73rt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const port = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
 async function run() {
   try {
-    await client.connect();
-    
-    // ended point
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    const database = client.db("TaskManager");
+    const usersCollection = database.collection("users");
+    const taskCollection = database.collection("task");
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-// connect to server
+
+
+app.get('/', (req, res) => {
+  res.send("Yo'Task Running")
+})
+
 app.listen(port, () => {
-  console.log(`App is listening at port ${port}`);
-});
+  console.log("Server Runnig", port);
+})
